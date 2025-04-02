454efd62f992e2f904ccbaea5cfe1993f06c4867
mjb90 <manasborah90@gmail.com>
2025-04-02 22:54:06 +0800

    Test Bob balance

--- script/DeployOurToken.s.sol
+++ script/DeployOurToken.s.sol
@@ -1,14 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import {OurToken} from "../src/OurToken.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployOurToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
 
-    function run() external {
+    function run() external returns (OurToken) {
         vm.startBroadcast();
-        new OurToken(INITIAL_SUPPLY);
+        OurToken ourToken = new OurToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
+        return ourToken;
     }
 }
